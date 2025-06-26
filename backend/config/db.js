606864a867811854f90d217d11fc2fbedc17c57d
Savio <155758557+savi0tomy@users.noreply.github.com>
2025-06-27 00:31:11 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://savio:UjqK13PmTRQqww1W@smartpact.jt9gh.mongodb.net/?retryWrites=true&w=majority&appName=smartpact");
+        await mongoose.connect("");
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
